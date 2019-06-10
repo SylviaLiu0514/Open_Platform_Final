78496808a26976476f454f9ccaab2e016a6c0e8f
tang03130313 <smile03131@gmail.com>
2019-06-11 00:53:00 +0800

    讀train.csv的資料

---
+++ README.md
@@ -0,0 +1,4 @@
+Open Platform Final
+====
+# read_csv_data
+##讀train.csv的資料 
---
+++ read_csv_data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
+
+def Read_train_data():
+    train_data = pd.read_csv('train.csv')
+    ID = train_data.pop('ID')
+    species = train_data.pop('Class')
+    return ID, species
+                              
+ID, species = Read_train_data()
+
+print(ID)
+print(species)
