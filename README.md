1c50566cf789996449d8e1335e6ea4c5c8750b8c
tang03130313 <smile03131@gmail.com>
2019-06-11 23:31:39 +0800

    read csv and image final

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Open Platform Final
 ====
+# load_image.py
+## 讀spectrogram資料夾內的所有頻譜(.png)
 # read_csv_data.py
-## 讀train.csv的資料 
+## 讀train.csv的資料和load_image.py讀進的圖片放一起
--- read_csv_data.py
+++ read_csv_data.py
@@ -2,14 +2,29 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
 from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
+import load_image
 
 def Read_train_data():
+    # Read train.csv and pop out the id
     train_data = pd.read_csv('train.csv')
     ID = train_data.pop('ID')
+    # Pop out the species and make species name correspond to number 
     species = train_data.pop('Class')
+    species = LabelEncoder().fit(species).transform(species)
+    # Standardize the data by setting the mean to 0 and std to 1
     return ID, species
                               
-ID, species = Read_train_data()
+def load_train_data():
+    # Load the train data
+    ID, label = Read_train_data()
+    # Load the image data
+    train_image = load_image.Load_image(ID)
+    # Split them into validation and cross-validation
+    sss = StratifiedShuffleSplit(n_splits=1, train_size=0.8, test_size=0.2)
+    train_id, test_id = next(sss.split(train_image, label))
+    tra_image, tra_label = train_image[train_id], label[train_id]
+    val_image, val_label = train_image[test_id], label[test_id]
+    return (tra_image, tra_label), (val_image, val_label)
 
-print(ID)
-print(species)
+#print(load_train_data())
+(tra_image, tra_label), (val_image, val_label) = load_train_data()
