52642542999696d168b3d775fc280d48048bb078
Sylvialiu0514 <sylvialiu0514@gmail.com>
2019-06-11 13:45:38 +0800

    fix

--- read_csv_data.py
+++ read_csv_data.py
@@ -7,18 +7,6 @@ from keras.models import Model, Sequential
 from keras.optimizers import SGD, RMSprop, Adam
 from PIL import Image
 
-def Load_image(ID, image_length = 512):
-    output_image = np.empty((len(ID), image_length, image_length, 1))
-    for i, image_ID in enumerate(ID):
-        image = img_to_array(resize_image(load_img('spectrogram/' + str(image_ID) + '.png', color_mode = "grayscale"), 
-                                                    image_length=image_length))
-
-        h1 = int((image_length - image.shape[0] ) / 2)
-        h2 = h1 + image.shape[0]
-        w1 = int((image_length - image.shape[1]) / 2)
-        w2 = w1 + image.shape[1]
-        output_image[i, h1:h2, w1:w2, 0:1] = image
-    return np.around(output_image / 255.0)
 
 def Read_train_data():
     train_data = pd.read_csv('train.csv')
